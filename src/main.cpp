b29a6c35bb72015e536790ea6d152ef4fa16295e
acidhood <nazar.kondrashov616@gmail.com>
2026-04-19 23:54:05 +0200

    fix_bag

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,22 @@
 #include "PhotoEditorQT.h"
 #include <QtWidgets/QApplication>
+#include <QSharedMemory>
+#include <QMessageBox>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    
+    QSharedMemory sharedMemory("acidhood999_PhotoEditorQT_UniqueKey");
+
+    if (sharedMemory.attach())
+    {
+        QMessageBox::warning(nullptr, "Warning", "The application is already launched!");
+        return 0;
+    }
+
+    if (!sharedMemory.create(1)) return 0;
+
     PhotoEditorQT window;
     window.show();
     return app.exec();
